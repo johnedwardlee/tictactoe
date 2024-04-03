d97e89455a174a778e6bbf20ab37519fc021b494
John Lee <50990111+johnedwardlee@users.noreply.github.com>
2024-04-02 21:07:00 -0400

    Created GameManager to handle turn switching and playing rounds in console

--- script.js
+++ script.js
@@ -25,10 +25,12 @@ const Gameboard = (function () {
     }
   }
 
-  // remove row col later and base it off of which cell is clicked
+  // Board getter function, used by UI manager to read and display the game
+  const getBoard = () => board;
+
+  // Place a Player's piece on the board, Inputs will be determined by which cell is clicked and whose turn it is
   const placePiece = (piece, row, col) => {
     board[row][col].setValue(piece);
-    printBoard();
   };
 
   const printBoard = () => {
@@ -39,6 +41,64 @@ const Gameboard = (function () {
   return {
     printBoard,
     placePiece,
+    getBoard,
+  };
+})();
+
+const GameManager = (function () {
+  const players = [
+    {
+      name: "Player 1",
+      piece: "X",
+    },
+    {
+      name: "Player 2",
+      piece: "O",
+    },
+  ];
+
+  let activePlayer = players[0];
+  const setPlayerName = (playerNum, newName) => {
+    if (typeof newName === "string" && (playerNum === 1 || playerNum === 0)) players[playerNum].name = newName;
+    else console.error("Error: Invalid input to setPlayerName");
+  };
+
+  const nextPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printRound = () => {
+    Gameboard.printBoard();
+    console.log(`${getActivePlayer().name}'s turn to place piece ${getActivePlayer().piece}`);
+  };
+
+  const playRound = (row, col) => {
+    // Make better error handling based on sizeof Gameboard.getBoard() or something like that
+    if (row >= rows || row < 0) {
+      console.error("Error: Row value out of bounds");
+      return;
+    }
+    if (col >= cols || col < 0) {
+      console.error("Error: Column value out of bounds");
+      return;
+    }
+    console.log(`${getActivePlayer().name} places a ${getActivePlayer().piece} at row:${row} col:${col}!`);
+    Gameboard.placePiece(getActivePlayer().piece, row, col);
+    // game winning logic
+    nextPlayerTurn();
+    printRound();
+  };
+
+  // Print initial game message
+  printRound();
+
+  return {
+    setPlayerName,
+    nextPlayerTurn,
+    getActivePlayer,
+    playRound,
   };
 })();
 
